13df22756afd965a4ff8e2ccd73294a88cef90f6
abraham <abrahamsuarezolay@gmail.com>
2023-03-12 15:49:33 +0000

    Basic Html and sections. Integrating boostrap as well

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import "./index.css";
 import Navbar from "./Components/NavBar";
 import About from "./Components/About";
 import Projects from "./Components/Projects";
+import Contact from "./Components/Contact";
 
 function App() {
 
@@ -9,6 +10,7 @@ function App() {
     <Navbar />
     <About />
     <Projects />
+    <Contact />
   </div>)
   
 }
