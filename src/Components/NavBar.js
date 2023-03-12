d841462e271e5e4945489592b7822d38fa0d3ebe
abraham <abrahamsuarezolay@gmail.com>
2023-03-12 14:32:06 +0000

    First commit

---
+++ src/Components/NavBar.js
@@ -0,0 +1,22 @@
+import React from "react";
+import { Link } from "react-scroll";
+
+
+const Navbar = () => {
+    return(<div className="navbar-container" id="home">
+        <header>
+            <nav>
+                <ul>
+                    <li><Link to="home" spy={true} smooth={true} offset={0} duration={500}>Home</Link></li>
+                    <li><Link to="about" spy={true} smooth={true} offset={0} duration={500}>About</Link></li>
+                    <li><Link to="projects" spy={true} smooth={true} offset={0} duration={500}>Projects</Link></li>
+                    <li><Link to="contact" spy={true} smooth={true} offset={0} duration={500}>Contact</Link></li>
+                </ul>
+            </nav>
+        </header>
+    </div>)
+
+}
+
+
+export default Navbar;
---
+++ src/Components/Projects.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const Projects = () => {
+    return(<div className="projects" id="projects">
+        <h1>Projects</h1>
+    </div>)
+}
+
+
+export default Projects;
